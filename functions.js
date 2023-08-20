b09f8763456e98d1bef724a916d0e728c34706f1
andrew-pyrookie <andrewthepyrookie@gmail.com>
2023-08-20 20:42:30 +0300

    2nd commit

--- functions.js
+++ functions.js
@@ -1,3 +1,22 @@
 function oncalculator(){
     document.getElementById("msg").innerHTML = "Calculator is ON. press OFF to shutdown";
+    
+    document.addEventListener("click",clearScreen )
+}
+
+function offcalculator(){
+    document.getElementById("msg").innerHTML = "Calculator is OFF. press ON to startup";
+    document.addEventListener("click",restorescreen )
+}
+
+const resultscreen = document.getElementById("resultScreen");
+const p = resultscreen.querySelector("p");
+
+function clearScreen(){
+    p.innerHTML="";
+
+}
+
+function restorescreen(){
+    p.innerHTML = "8888888888888888888888888888888888";
 }
